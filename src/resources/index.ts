89ead91168a2dbd133bc3d5ed3b34be904674a76
Adarsh Pastakia <adarshpastakia@outlook.com>
2016-12-10 16:48:51 +0400

    Added index.ts

---
+++ src/resources/index.ts
@@ -0,0 +1,52 @@
+import {FrameworkConfiguration} from 'aurelia-framework';
+
+import 'lodash';
+import 'moment';
+import 'numeral';
+
+import './libs/window';
+import './libs/phonelib';
+import './libs/countries';
+import './libs/currencies';
+import './libs/filetypes';
+
+import * as ld from "lodash";
+import * as km from "kramed";
+import * as mm from "moment";
+import * as nm from "numeral";
+
+export var _ = ld;
+export var kramed = km;
+export var moment = mm;
+export var numeral = nm;
+
+export * from './utils/ui-utils';
+export * from './utils/ui-event';
+
+import './elements/core/ui-viewport';
+
+import './elements/core/ui-grid';
+import './elements/core/ui-page';
+
+import './elements/inputs/ui-form';
+import './elements/inputs/ui-input';
+import './elements/inputs/ui-textarea';
+import './elements/inputs/ui-dropdown';
+
+export function configure(config: FrameworkConfiguration) {
+  // Core Elements
+  config.globalResources([
+    './elements/core/ui-viewport',
+    './elements/core/ui-grid',
+    './elements/core/ui-page'
+  ]);
+  // Input Elements
+  config.globalResources([
+    './elements/inputs/ui-form',
+    './elements/inputs/ui-input',
+    './elements/inputs/ui-phone',
+    './elements/inputs/ui-options',
+    './elements/inputs/ui-textarea',
+    './elements/inputs/ui-dropdown'
+  ]);
+}
