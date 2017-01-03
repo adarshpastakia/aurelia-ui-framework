4809e73e2ab7c0002e38219d3d1a3de192fa966d
Adarsh Pastakia <adarshpastakia@outlook.com>
2017-01-03 14:39:04 +0400

    Updated examples

--- src/api/app.ts
+++ src/api/app.ts
@@ -18,4 +18,44 @@ export class ApiApp {
   unbind() { }
   deactivate() { }
   // end aurelia hooks
+
+
+  usageExample = `\`\`\`ts
+import {autoinject} from "aurelia-framework";
+import {UIApplication} from "aurelia-ui-framework";
+
+@autoinject()
+export class PageView {
+  constructor(public app:UIApplication){}
+}
+  \`\`\``;
+
+  userModel = `\`\`\`ts
+UserModel {
+  username:string;
+  password:string;
+  token:string;
+}
+  \`\`\``;
+
+  toastConfig = `\`\`\`ts
+ToastConfig {
+  title:string;
+  message:string;
+  glyph:string;
+  theme:string;
+  timeout:number; // 0 disable auto-hide
+}
+  \`\`\``;
+
+  alertConfig = `\`\`\`ts
+AlertConfig {
+  title:string;
+  message:string;
+  glyph:string;
+  okLabel:string;
+  cancelLabel:string;
+}
+  \`\`\``;
+
 }
