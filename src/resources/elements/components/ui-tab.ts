f172c3d50913d3c038c6de78d58ed73c900c1602
Adarsh Pastakia <adarshpastakia@outlook.com>
2017-03-16 14:25:48 +0400

    Added extra sections and overflow menu for tab bar

--- src/resources/elements/components/ui-tab.ts
+++ src/resources/elements/components/ui-tab.ts
@@ -4,15 +4,88 @@
 // @copyright   : 2017
 // @license     : MIT
 import {autoinject, customElement, bindable, bindingMode, children, inlineView, useView, containerless, View, DOM} from 'aurelia-framework';
+import {UIUtils} from "../../utils/ui-utils";
 import {UIEvent} from "../../utils/ui-event";
+import * as _ from "lodash";
+
+@containerless()
+@customElement('ui-tabbar-start')
+@inlineView(`<template><div slot="ui-tabbar-start" class="ui-tabbar-links"><slot></slot></div></template>`)
+export class UITabbarStart { }
+
+@containerless()
+@customElement('ui-tabbar-end')
+@inlineView(`<template><div slot="ui-tabbar-end" class="ui-tabbar-links"><slot></slot></div></template>`)
+export class UITabbarEnd { }
+
+@customElement('ui-tabbar-toggle')
+@inlineView(`<template class="ui-tabbar-toggle ui-tab-button \${disabled?'ui-disabled':''}" click.trigger="toggleDropdown($event)"><slot></slot></template>`)
+export class UITabbarToggle {
+  @bindable() dropdown;
+  @bindable() disabled = false;
+
+  private tether;
+  private obMouseup;
+  isDisabled = false;
+
+  constructor(public element: Element) { }
+
+  attached() {
+    if (this.dropdown) {
+      this.obMouseup = UIEvent.subscribe('mouseclick', (evt) => {
+        if (getParentByClass(evt.target, 'ui-button') == this.element) return;
+        this.element.classList.remove('ui-open');
+        this.dropdown.classList.remove('ui-open');
+      });
+      this.element.classList.add('ui-btn-dropdown');
+      this.dropdown.classList.add('ui-floating');
+      this.tether = UIUtils.tether(this.element, this.dropdown);
+    }
+  }
+  detached() {
+    if (this.tether) this.tether.dispose();
+    if (this.obMouseup) this.obMouseup.dispose();
+    if (this.dropdown) DOM.removeNode(this.dropdown);
+  }
+
+  toggleDropdown(evt) {
+    if (evt.button != 0) return true;
+    if (this.dropdown) {
+      evt.preventDefault();
+      evt.stopPropagation();
+      evt.cancelBubble = true;
+      if (this.element.classList.contains('ui-open')) {
+        UIEvent.fireEvent('menuhide', this.element);
+        this.element.classList.remove('ui-open');
+        this.dropdown.classList.remove('ui-open');
+      }
+      else {
+        if (UIEvent.fireEvent('menuopen', this.element) !== false) {
+          this.element.classList.add('ui-open');
+          this.dropdown.classList.add('ui-open');
+          this.tether.position();
+        }
+      }
+      return false;
+    }
+    return true;
+  }
+
+}
 
 @autoinject()
-@inlineView(`<template class="ui-tab-panel" css.bind="{'min-height': height}"><div class="ui-tab-bar" if.bind="!noTabs">
+@inlineView(`<template class="ui-tab-panel" css.bind="{'min-height': height}"><div class="ui-tabbar">
+  <slot name="ui-tabbar-start"></slot>
+  <div class="ui-tabbar-buttons" ref="wrapper" if.bind="!noTabs">
     <a click.trigger="activateTab(tab)" repeat.for="tab of tabs" class="ui-tab-button \${tab.active?'ui-active':''} \${tab.disabled?'ui-disabled':''}">
-      <span if.bind="tab.glyph" class="fi-ui-\${tab.glyph}"></span>
+      <ui-glyph if.bind="tab.glyph" class="ui-tab-icon \${tab.glyph}" glyph.bind="tab.glyph"></ui-glyph>
       <span class="ui-label">\${tab.label}</span>
       <span if.bind="tab.closeable" class="ui-close" click.trigger="closeTab(tab)">&nbsp;&times;</span>
     </a>
+    <div class="ui-tabbar-toggle ui-tab-button" ref="overflowToggle" show.bind="isOverflow" click.trigger="showOverflow($event)"><ui-glyph glyph="ui-handle-overflow"></ui-glyph></div>
+  </div>
+  <slot name="ui-tabbar-end"></slot>
+  <div class="ui-menu ui-tabbar-overflow ui-floating" ref="overflow"></div>
   </div><slot></slot></template>`)
 @customElement('ui-tab-panel')
 export class UITabPanel {
@@ -25,31 +98,56 @@ export class UITabPanel {
   // aurelia hooks
   created(owningView: View, myView: View) { }
   bind(bindingContext: Object, overrideContext: Object) { }
-  attached() { }
-  detached() { }
+  attached() {
+    if (!this.noTabs) {
+      this.obResize = UIEvent.subscribe('windowresize', () => this.arrange());
+      this.obClick = UIEvent.subscribe('mouseclick', () => this.overflow.classList.remove('ui-open'));
+      this.tether = UIUtils.tether(this.overflowToggle, this.overflow, { resize: false, position: 'br' });
+      window.setTimeout(() => this.arrange(), 500);
+    }
+  }
+  detached() {
+    if (!this.noTabs) {
+      this.tether.dispose();
+      this.obClick.dispose();
+      this.obResize.dispose();
+    }
+  }
   unbind() { }
   // end aurelia hooks
 
+  private tether;
+  private isOverflow = false;
+
+  private wrapper: Element;
+  private overflow: Element;
+  private overflowToggle: Element;
+
+  private obClick;
+  private obResize;
+
   @bindable() height = "auto";
   @children('ui-tab') tabs = [];
-  @bindable({ defaultBindingMode: bindingMode.twoWay }) activeTab = 0;
+  @bindable({ defaultBindingMode: bindingMode.twoWay }) activeTab: any = 0;
 
   private noTabs = false;
   private activeTabEl;
 
   private tabsChanged() {
     if (this.tabs.length > 0 && _.find(this.tabs, ['active', true]) == null)
-      (this.activeTabEl = _.find(this.tabs, ['disabled', false])).active = true;
+      (this.activeTabEl = _.findLast(this.tabs, ['disabled', false])).active = true;
   }
 
   private activeTabChanged(newValue) {
     if (this.tabs.length == 0) return;
+    let tab = (_.find(this.tabs, ['id', newValue]) || this.tabs[newValue] || this.activeTabEl);
     if (this.activeTabEl) this.activeTabEl.active = false;
-    (this.activeTabEl = (this.tabs[newValue] || this.activeTabEl)).active = true;
+    (this.activeTabEl = tab).active = true;
   }
 
   private closeTab(tab) {
     if (UIEvent.fireEvent('beforeclose', this.element, tab)) {
+      _.remove(this.tabs, ['id', tab.id]);
       tab.remove();
       UIEvent.fireEvent('close', this.element, tab);
     }
@@ -59,9 +157,35 @@ export class UITabPanel {
     if (UIEvent.fireEvent('beforechange', this.element, tab)) {
       if (this.activeTabEl) this.activeTabEl.active = false;
       (this.activeTabEl = tab).active = true;
+      this.activeTab = tab.id;
       UIEvent.fireEvent('change', this.element, tab);
     }
   }
+
+  private arrange() {
+    this.overflow.classList.remove('ui-open');
+    for (let i = 0, c = this.overflow['children']; i < c.length; i++) {
+      this.wrapper.insertBefore(c[i], this.overflowToggle);
+    }
+    // this.wrapper.appendChild(this.overflowToggle);
+    if (this.tabs.length > 0 && (this.isOverflow = (this.wrapper.lastElementChild.previousElementSibling.offsetLeft + this.wrapper.lastElementChild.previousElementSibling.offsetWidth > this.wrapper.offsetWidth))) {
+      for (let c = this.wrapper['children'], i = c.length - 2; i >= 0; i--) {
+        if (c[i].offsetLeft + c[i].offsetWidth > this.wrapper.offsetWidth) {
+          if (this.overflow.hasChildNodes) this.overflow.insertBefore(c[i], this.overflow.childNodes[0]); else this.overflow.appendChild(c[i]);
+        }
+      }
+    }
+  }
+  private showOverflow(evt) {
+    if (evt.button != 0) return true;
+    if (!this.overflow.classList.contains('ui-open')) {
+      this.overflow.classList.add('ui-open');
+      this.tether.position();
+    }
+    else
+      this.overflow.classList.remove('ui-open');
+  }
+
 }
 
 @autoinject()
