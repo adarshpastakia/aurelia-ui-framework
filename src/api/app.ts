66ddba65d5cbd8df7c7716f7a91fcfb9897f58b4
Adarsh Pastakia <adarshpastakia@outlook.com>
2017-03-30 04:16:24 +0400

    Updated

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,5 +1,5 @@
-// 
-// @description : 
+//
+// @description :
 // @author      : Adarsh Pastakia
 // @copyright   : 2017
 // @license     : MIT
@@ -18,44 +18,5 @@ export class ApiApp {
   unbind() { }
   deactivate() { }
   // end aurelia hooks
-
-
-  usageExample = `\`\`\`ts
-import {autoinject} from "aurelia-framework";
-import {UIApplication} from "aurelia-ui-framework";
-
-@autoinject()
-export class PageView {
-  constructor(public app:UIApplication){}
-}
-  \`\`\``;
-
-  userModel = `\`\`\`ts
-UserModel {
-  username:string;
-  password:string;
-  token:string;
-}
-  \`\`\``;
-
-  toastConfig = `\`\`\`ts
-ToastConfig {
-  title:string;
-  message:string;
-  glyph:string;
-  theme:string;
-  timeout:number; // 0 disable auto-hide
-}
-  \`\`\``;
-
-  alertConfig = `\`\`\`ts
-AlertConfig {
-  title:string;
-  message:string;
-  glyph:string;
-  okLabel:string;
-  cancelLabel:string;
-}
-  \`\`\``;
 
 }
