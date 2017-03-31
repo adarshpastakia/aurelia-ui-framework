bcbb6090aa6981aaba7a5075901f57d318c29fe3
Adarsh Pastakia <adarshpastakia@outlook.com>
2017-03-31 04:16:53 +0400

    Fixed issue when resetting value

--- src/resources/elements/components/ui-dropdown.ts
+++ src/resources/elements/components/ui-dropdown.ts
@@ -59,7 +59,7 @@ export class UIDropdown {
 
     let it = this.items.find(it => it.value == newValue);
     if (!it) it = this.items[0];
-    this.value = it.value;
+    if (it.value != newValue) this.value = it.value;
     this.display = it.element.innerText;
     this.glyph = it.element.au.controller.viewModel.glyph;
     (this.selected = it).element.classList.add('ui-selected');
