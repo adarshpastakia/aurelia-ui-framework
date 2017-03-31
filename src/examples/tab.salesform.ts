05fa56eb66a0e82dd03a086bb677a581d42f57f7
Adarsh Pastakia <adarshpastakia@outlook.com>
2017-03-31 04:21:43 +0400

    Examples Updated

--- src/examples/tab.salesform.ts
+++ src/examples/tab.salesform.ts
@@ -32,21 +32,49 @@ export class TabSalesForm {
       this.isSaving = false;
 
       UIUtils.confirm("Add another sale?")
-        .then(b => b ? null : UIEvent.fireEvent('close', this.element));
+        .then(b => b ? this.resetForm() : UIEvent.fireEvent('close', this.element));
     }, 1000);
   }
 
-  prefix = "900 001";
-  prefixCls = "ui-bg-lime";
-  changeType(type) {
-    if (type == 0) this.prefix = "900 001";
-    if (type == 1) this.prefix = "800 001";
-    if (type == 2) this.prefix = "600 001";
-    if (type == 3) this.prefix = "500 001";
+  resetForm() {
+    this.type = 0;
+  }
 
-    if (type == 0) this.prefixCls = "ui-bg-yellow";
-    if (type == 1) this.prefixCls = "ui-bg-blue ui-text-white";
-    if (type == 2) this.prefixCls = "ui-bg-teal ui-text-white";
-    if (type == 3) this.prefixCls = "ui-bg-purple ui-text-white";
+  type = 0;
+  amount = 0;
+  ship = 0;
+  discount = 0;
+
+  prefix = "900 001";
+  prefixCls = "ui-bg-yellow";
+  changeType(type) {
+    if (type == 0) {
+      this.amount = 0;
+      this.ship = 0;
+      this.discount = 0;
+      this.prefix = "900 001";
+      this.prefixCls = "ui-bg-yellow";
+    }
+    if (type == 1) {
+      this.amount = 39.99;
+      this.ship = 10.00;
+      this.discount = -4.99;
+      this.prefix = "800 001";
+      this.prefixCls = "ui-bg-blue ui-text-white";
+    }
+    if (type == 2) {
+      this.amount = 69.99;
+      this.ship = 10.00;
+      this.discount = -7.99;
+      this.prefix = "600 001";
+      this.prefixCls = "ui-bg-teal ui-text-white";
+    }
+    if (type == 3) {
+      this.amount = 99.99;
+      this.ship = 0.00;
+      this.discount = -9.99;
+      this.prefix = "500 001";
+      this.prefixCls = "ui-bg-purple ui-text-white";
+    }
   }
 }
