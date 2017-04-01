14ca5d7be44ada437e9557628a93a3a6692dac42
Adarsh Pastakia <adarshpastakia@outlook.com>
2017-04-01 15:50:06 +0400

    Added element to display label/value as removable chips
    <ui-chip label="label">value</ui-chip>

--- src/resources/elements/components/ui-breadcrumb.ts
+++ src/resources/elements/components/ui-breadcrumb.ts
@@ -7,6 +7,36 @@ import {autoinject, customElement, bindable, bindingMode, children, inlineView, 
 import {UIEvent} from "../../utils/ui-event";
 
 @autoinject()
+@inlineView(`<template class="ui-chip"><span class="ui-chip-label" css.bind="{'background-color':color}">\${label}</span><span class="ui-chip-value"><slot></slot></span><a click.trigger="remove()" class="ui-chip-close" if.bind="canClose">&times</a></template>`)
+@customElement('ui-chip')
+export class UIChip {
+  constructor(public element: Element) {
+    if (element.hasAttribute('big')) element.classList.add('big');
+    if (element.hasAttribute('small')) element.classList.add('small');
+
+    this.canClose = element.hasAttribute('removable');
+  }
+
+  // aurelia hooks
+  // created(owningView: View, myView: View) { }
+  // bind(bindingContext: Object, overrideContext: Object) { }
+  // attached() { }
+  // detached() { }
+  // unbind() { }
+  // end aurelia hooks
+
+  @bindable() id = '';
+  @bindable() label = '';
+  @bindable() color = '';
+
+  private canClose = false;
+
+  remove() {
+    UIEvent.fireEvent('remove', this.element, this.id);
+  }
+}
+
+@autoinject()
 @inlineView(`<template class="ui-breadcrumb" crumbclicked.delegate="fireChange($event)"><slot></slot></template>`)
 @customElement('ui-breadcrumb')
 export class UIBreadcrumb {
