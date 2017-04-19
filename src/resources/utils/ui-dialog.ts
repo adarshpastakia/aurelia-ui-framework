2e7a4cd848a1cabab0ea48ecb5ca0a5fea2a1806
Adarsh Pastakia <adarshpastakia@outlook.com>
2017-04-19 13:15:57 +0400

    Fixed #11

--- src/resources/utils/ui-dialog.ts
+++ src/resources/utils/ui-dialog.ts
@@ -116,7 +116,7 @@ export class UIDialogService {
 
       dialog.taskButtonEl = document.createElement('button');
       dialog.taskButtonEl.classList.add('ui-active');
-      dialog.taskButtonEl.innerHTML = '<ui-glyph class="${glyph}" glyph="${glyph}"></ui-glyph>&nbsp;<span class="ui-label">${title}</span>';
+      dialog.taskButtonEl.innerHTML = '<ui-glyph class="${glyph}" glyph="${glyph}" if.bind="glyph"></ui-glyph><span class="ui-label">${title}</span>';
       dialog.taskButtonEl.window = dialog;
       if (UIUtils.taskbarContainer) {
         UIUtils.taskbarContainer.appendChild(dialog.taskButtonEl);
