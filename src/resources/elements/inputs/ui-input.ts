63506ef868c0662b2376fa97612a200e056c9ad5
Adarsh Pastakia <adarshpastakia@outlook.com>
2017-06-09 14:49:20 +0400

    Fixed issue when value is '0'

--- src/resources/elements/inputs/ui-input.ts
+++ src/resources/elements/inputs/ui-input.ts
@@ -123,21 +123,21 @@ export class UIInput extends UIBaseInput {
   valueChanged(newValue) {
     if (this.ignore) return;
     this.ignore = true;
-    this.number = isNaN(parseFloat(newValue)) ? null : parseFloat(newValue);
-    this.decimal = isNaN(parseFloat(newValue)) ? null : parseFloat(newValue);
+    this.number = isNaN(newValue) ? null : parseFloat(newValue);
+    this.decimal = isNaN(newValue) ? null : parseFloat(newValue);
     if (this.type === 'number' && this.number === null && this.decimal === null) this.inputEl.value = this.value = '';
     setTimeout(() => this.ignore = false, 100);
   }
   numberChanged(newValue) {
     if (this.ignore) return;
     this.ignore = true;
-    this.value = newValue === null ? '' : newValue;
+    this.inputEl.value = this.value = newValue === null ? '' : newValue;
     setTimeout(() => this.ignore = false, 100);
   }
   decimalChanged(newValue) {
     if (this.ignore) return;
     this.ignore = true;
-    this.value = newValue === null ? '' : newValue;
+    this.inputEl.value = this.value = newValue === null ? '' : newValue;
     setTimeout(() => this.ignore = false, 100);
   }
 
