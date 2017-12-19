7c94ac162cf0e93e02583fe734160819e6d0b7c1
Adarsh Pastakia <adarshpastakia@outlook.com>
2017-12-19 11:22:17 +0530

    Row select functionality

--- src/elements/components/ui-datagrid.ts
+++ src/elements/components/ui-datagrid.ts
@@ -91,7 +91,7 @@ export class BodyCell {
   <div class="ui-dg-cell ui-row-head" css.bind="{width: parent.counterWidth+'px'}" if.bind="parent.rowCounter">
     <div class="ui-dg-cell-content ui-text-center">\${(index+1) + (parent.dataSource.recordsPerPage * parent.dataSource.page)}</div>
   </div>
-  <div class="ui-dg-cell ui-cell-checkbox" click.trigger="parent.toggleRecordCheck(record)">
+  <div class="ui-dg-cell ui-cell-checkbox" click.trigger="parent.toggleRecordCheck($event,record)">
     <ui-glyph glyph.bind="record.__selected__?'glyph-tree-check-on':'glyph-tree-check-off'"></ui-glyph>
   </div>
   <body-cell repeat.for="column of parent.colLocked" record.bind="record" column.bind="column"></body-cell>
@@ -139,7 +139,7 @@ export class BodyRow {
     <div class="ui-dg-cell last-cell"><div class="ui-dg-cell-content">&nbsp;</div></div>
   </div>
 </div>
-<div class="ui-dg-body" scroll.trigger="scrollLeft = $event.target.scrollLeft">
+<div class="ui-dg-body \${rowSelect?'ui-row-hilight':''}" scroll.trigger="scrollLeft = $event.target.scrollLeft">
   <body-row repeat.for="record of dataSource.data" record.bind="record" if.bind="!virtual" click.trigger="fireSelect($event, record)"></body-row>
   <div class="ui-dg-row ui-last-row">
     <div class="ui-dg-lock-group" css.bind="{transform: 'translateX('+(scrollLeft)+'px)'}">
@@ -157,7 +157,7 @@ export class BodyRow {
 export class UIDatagrid {
   constructor(public element: Element, private engine: TemplatingEngine) {
     this.virtual = element.hasAttribute('virtual');
-    this.rowSelect = element.hasAttribute('rowselect');
+    this.rowSelect = element.hasAttribute('rowselect.trigger');
     this.rowCheckbox = element.hasAttribute('row-checkbox');
     this.rowCounter = element.hasAttribute('row-counter');
     this.rowExpander = element.hasAttribute('row-expander');
@@ -215,7 +215,9 @@ export class UIDatagrid {
     this.obPageChange = UIEvent.observe(this.dataSource, 'data', () => this.selectedRows = []);
   }
 
-  toggleRecordCheck(record) {
+  toggleRecordCheck($event, record) {
+    $event.stopPropagation();
+    $event.preventDefault();
     record.__selected__ = !record.__selected__;
     this.selectedRows = _.filter(this.dataSource.data, ['__selected__', true]);
   }
