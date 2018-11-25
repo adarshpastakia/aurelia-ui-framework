70eb20c316ece673e68766cb98112a049edcba79
Adarsh Pastakia <adarshpastakia@outlook.com>
2018-11-25 19:57:44 +0400

    Update for list input logic

--- src/forms/list-input.ts
+++ src/forms/list-input.ts
@@ -13,11 +13,11 @@ import { autoinject, containerless, inlineView, processContent } from "aurelia-f
   <template if.bind="multiple">
     <div class="ui-tag" repeat.for="m of model">
       <span with.bind="{m}" show.one-time="buildOption(m, __el, true) & debounce" ref="__el"></span>
-      <span class="ui-tag__close" click.trigger="removeValue(m)">&#x00D7;</span></div>
+      <span class="ui-tag__close" click.trigger="removeOption(m)">&#x00D7;</span></div>
   </template>
   <input ref="$parent.inputEl" role="combo" size="1" placeholder.bind="placeholder" disabled.bind="disabled || isDisabled || isPlain"
     readonly.bind="readonly" value.two-way="inputValue" input.trigger="filterOptions() & debounce" keydown.trigger="checkKeyEvent($event)"
-    change.trigger="false" focus.trigger="toggleDrop(true)" blur.trigger="[canToggleDrop($event), resetQuery()]" />
+    change.trigger="false" focus.trigger="toggleDrop(true)" blur.trigger="[canToggleDrop($event), resetQuery(true)] & debounce" />
   </div>
 </template>`)
 @processContent((compiler, resources, node, instruction) => {
--- src/forms/list-maker.ts
+++ src/forms/list-maker.ts
@@ -44,6 +44,7 @@ export class ListMaker extends BaseInput {
   protected isLoaded = false;
   protected isLoading = false;
   protected isGrouped = false;
+  protected isFiltered = false;
   protected ignoreChange = false;
 
   protected hilightIndex = -1;
@@ -77,28 +78,47 @@ export class ListMaker extends BaseInput {
           if (this.matcher) {
             return this.matcher({ option: o, value: this.value });
           } else {
-            return this.value.includes(o[this.valueProperty] || o);
+            return this.value === (o[this.valueProperty] || o);
           }
         });
-        this.resetQuery();
       }
     } else {
+      this.model = null;
       this.inputValue = "";
-      if (!this.dropEl) {
+    }
+    if (!this.dropEl) {
+      UIInternal.queueTask(() => {
+        const selected = this.listContainer.querySelector(".ui-list__item--selected");
+        if (selected) {
+          selected.scrollIntoView({ block: "nearest" });
+        }
+      });
+    }
+    this.resetQuery();
+  }
+
+  protected toggleDrop(open?: boolean): boolean {
+    if (this.dropEl) {
+      if (open === true && this.dropEl.isOpen) {
+        UIInternal.queueMicroTask(() => this.dropEl.updatePosition());
+        return;
+      }
+      if (super.toggleDrop(open)) {
         this.loadOptions();
       }
     }
   }
 
-  protected clear(): void {
-    this.model = null;
-    this.value = null;
-    this.inputValue = "";
-    this.inputEl.focus();
-    this.loadOptions();
+  protected loadOptions() {
+    if (this.query) {
+      this.fetchOptions();
+    } else {
+      this.buildOptions(this.options);
+    }
   }
 
-  protected filterOptions(): void {
+  protected filterOptions() {
+    this.isFiltered = !!this.inputValue;
     if (this.query) {
       this.fetchOptions(this.inputValue);
     } else {
@@ -114,7 +134,7 @@ export class ListMaker extends BaseInput {
     }
   }
 
-  protected selectOption(model: AnyObject): void {
+  protected selectOption(model) {
     this.ignoreChange = true;
     this.hilightIndex = -1;
     if (this.multiple) {
@@ -132,78 +152,26 @@ export class ListMaker extends BaseInput {
       this.model = model;
       this.resetQuery();
     }
-    if (!this.dropEl && this.query) {
-      this.loadOptions();
-    }
     if (this.dropEl) {
       this.dropEl.closeDrop();
+    }
+    if (this.isFiltered) {
+      this.isFiltered = false;
+      this.loadOptions();
     }
     this.element.dispatchEvent(UIInternal.createEvent("change", this.value));
     this.element.dispatchEvent(UIInternal.createEvent("select", this.model));
     setTimeout(() => (this.ignoreChange = false), 500);
   }
 
-  protected removeValue(model: AnyObject): void {
+  protected removeOption(model) {
     this.ignoreChange = true;
     this.model = [...this.model.filter(m => m !== model)];
     this.value = this.value.filter(m => m !== (model[this.valueProperty] || model));
     setTimeout(() => (this.ignoreChange = false), 500);
   }
 
-  protected listClass(option, index): string {
-    const classes = ["ui-list__item"];
-    option.__selected = false;
-    if (!this.multiple) {
-      if (this.matcher) {
-        if (this.matcher({ option, value: this.value })) {
-          option.__selected = true;
-          classes.push("ui-list__item--selected");
-        }
-      } else if ((option[this.valueProperty] || option) === this.value) {
-        option.__selected = true;
-        classes.push("ui-list__item--selected");
-      }
-    } else if (this.multiple && this.value) {
-      if (this.matcher) {
-        this.value.forEach(value => {
-          if (this.matcher({ option, value })) {
-            option.__selected = true;
-            classes.push("ui-list__item--disabled");
-          }
-        });
-      } else if (this.value.includes(option[this.valueProperty] || option)) {
-        option.__selected = true;
-        classes.push("ui-list__item--disabled");
-      }
-    }
-    if (this.hilightIndex === index) {
-      classes.push("ui-list__item--hilight");
-    }
-    return classes.join(" ");
-  }
-
-  protected toggleDrop(open?: boolean): boolean {
-    if (this.dropEl) {
-      if (open === true && this.dropEl.isOpen) {
-        UIInternal.queueMicroTask(() => this.dropEl.updatePosition());
-        return;
-      }
-      if (super.toggleDrop(open)) {
-        this.loadOptions();
-      }
-    }
-  }
-
-  protected loadOptions(): void {
-    // TODO: Provide callback for fetching existing selected models
-    if (this.query) {
-      this.fetchOptions();
-    } else {
-      this.buildOptions(this.options);
-    }
-  }
-
-  private resetQuery(): void {
+  protected resetQuery(clearFilter?: boolean) {
     this.hilightIndex = -1;
     if (this.multiple) {
       this.inputValue = "";
@@ -212,7 +180,20 @@ export class ListMaker extends BaseInput {
         ? (this.model[this.labelProperty] || this.model).replace("<u>", "").replace("</u>", "")
         : "";
     }
-    // TODO: Reset options for filtered lists only
+    if (clearFilter && this.isFiltered) {
+      this.isFiltered = false;
+      this.loadOptions();
+    }
+  }
+
+  protected clear() {
+    this.model = null;
+    this.value = null;
+    this.inputValue = "";
+    this.inputEl.focus();
+    if (this.isFiltered) {
+      this.loadOptions();
+    }
   }
 
   private async fetchOptions(query?: string) {
@@ -264,12 +245,43 @@ export class ListMaker extends BaseInput {
     });
   }
 
+  private listClass(option, index): string {
+    const classes = ["ui-list__item"];
+    option.__selected = false;
+    if (!this.multiple) {
+      if (this.matcher) {
+        if (this.matcher({ option, value: this.value })) {
+          option.__selected = true;
+          classes.push("ui-list__item--selected");
+        }
+      } else if ((option[this.valueProperty] || option) === this.value) {
+        option.__selected = true;
+        classes.push("ui-list__item--selected");
+      }
+    } else if (this.multiple && this.value) {
+      if (this.matcher) {
+        this.value.forEach(value => {
+          if (this.matcher({ option, value })) {
+            option.__selected = true;
+            classes.push("ui-list__item--disabled");
+          }
+        });
+      } else if (this.value.includes(option[this.valueProperty] || option)) {
+        option.__selected = true;
+        classes.push("ui-list__item--disabled");
+      }
+    }
+    if (this.hilightIndex === index) {
+      classes.push("ui-list__item--hilight");
+    }
+    return classes.join(" ");
+  }
+
   private markOption(option: AnyObject): void {
     let lbl = option[this.labelProperty] || `${option}`;
     if (isEmpty(this.inputValue)) {
       return lbl;
     }
-    // options.forEach(o => {
     const rx = new RegExp(this.inputValue, "i");
     const asc = lbl.toString().ascii();
     if (rx.test(asc)) {
@@ -281,7 +293,6 @@ export class ListMaker extends BaseInput {
         "</u>" +
         lbl.substr(start + this.inputValue.length);
     }
-    // });
     return lbl;
   }
 
@@ -292,7 +303,10 @@ export class ListMaker extends BaseInput {
         ? this.template.outerHTML
         : `<template><div innerhtml.bind="$label"></div></template>`;
       const model = {
-        $label: unmark ? option[this.labelProperty] || option : this.markOption(option),
+        $label:
+          this.isFiltered && !unmark
+            ? this.markOption(option)
+            : option[this.labelProperty] || option,
         $model: option,
         $value: option[this.labelProperty] || option
       };
@@ -311,36 +325,39 @@ export class ListMaker extends BaseInput {
         this.dropEl.toggleDrop();
       }
       if ($event.keyCode === KEY_DOWN) {
-        let d = 1;
-        while (
-          this.hilightIndex + d !== this.innerOptions.length &&
-          (this.innerOptions[this.hilightIndex + d].__type === "group" ||
-            this.innerOptions[this.hilightIndex + d].__selected ||
-            this.innerOptions[this.hilightIndex + d].__disabled)
-        ) {
-          d++;
-        }
         this.hilightIndex =
-          this.hilightIndex + d < this.innerOptions.length ? this.hilightIndex + d : -1;
+          this.hilightIndex === -1 && this.model
+            ? this.innerOptions.indexOf(this.model)
+            : this.hilightIndex >= this.innerOptions.length
+            ? -1
+            : this.hilightIndex;
+        while (
+          this.hilightIndex + 1 !== this.innerOptions.length &&
+          (this.innerOptions[this.hilightIndex + 1].__type === "group" ||
+            this.innerOptions[this.hilightIndex + 1].__selected ||
+            this.innerOptions[this.hilightIndex + 1].__disabled)
+        ) {
+          this.hilightIndex++;
+        }
+        this.hilightIndex = this.hilightIndex + 1;
       }
       if ($event.keyCode === KEY_UP) {
-        let d = 1;
+        this.hilightIndex =
+          this.hilightIndex === -1 && this.model
+            ? this.innerOptions.indexOf(this.model)
+            : this.hilightIndex === -1
+            ? this.innerOptions.length
+            : this.hilightIndex;
+
         while (
-          this.hilightIndex - d > 0 &&
-          (this.innerOptions[this.hilightIndex - d].__type === "group" ||
-            this.innerOptions[this.hilightIndex - d].__selected ||
-            this.innerOptions[this.hilightIndex - d].__disabled)
+          this.hilightIndex - 1 > 0 &&
+          (this.innerOptions[this.hilightIndex - 1].__type === "group" ||
+            this.innerOptions[this.hilightIndex - 1].__selected ||
+            this.innerOptions[this.hilightIndex - 1].__disabled)
         ) {
-          d++;
+          this.hilightIndex--;
         }
-        if (
-          this.hilightIndex > 0 &&
-          !(this.innerOptions[this.hilightIndex - d].__type === "group" ||
-            this.innerOptions[this.hilightIndex - d].__selected ||
-            this.innerOptions[this.hilightIndex - d].__disabled)
-        ) {
-          this.hilightIndex = this.hilightIndex - d;
-        }
+        this.hilightIndex = this.hilightIndex - 1;
       }
       UIInternal.queueTask(() => {
         const selected = this.listContainer.querySelector(".ui-list__item--hilight");
@@ -355,7 +372,7 @@ export class ListMaker extends BaseInput {
     } else if (this.multiple && $event.keyCode === BACKSPACE) {
       if (this.model.length > 0 && this.inputValue.length === 0) {
         $event.stopEvent();
-        this.removeValue(this.model.last());
+        this.removeOption(this.model.last());
       }
     } else {
       this.fireEnter($event);
