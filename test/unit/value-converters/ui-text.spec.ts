14f8834f9902b603b8b7835b48faa049712909d5
Adarsh Pastakia <adarshpastakia@outlook.com>
2019-03-02 16:23:25 +0400

    Trying to fix testing on travis

--- test/unit/value-converters/ui-text.spec.ts
+++ test/unit/value-converters/ui-text.spec.ts
@@ -9,8 +9,6 @@ import { bootstrap } from "aurelia-bootstrapper";
 import { PLATFORM } from "aurelia-framework";
 import { StageComponent } from "aurelia-testing";
 
-jest.setTimeout(10000);
-
 describe("valueConverter/ui-text", () => {
   let component;
 
