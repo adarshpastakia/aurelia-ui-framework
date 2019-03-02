fc6ab2999a6cceb628bf574ff74f3dbd5453f84b
Adarsh Pastakia <adarshpastakia@outlook.com>
2019-03-02 15:32:06 +0400

    Trying to fix testing on travis

--- test/unit/value-converters/ui-text.spec.ts
+++ test/unit/value-converters/ui-text.spec.ts
@@ -20,6 +20,29 @@ describe("valueConverter/ui-text", () => {
       .inView(
         `<ui-viewport view-model.ref="viewport">
         <div id="json">\${{ a: "a" } | json}</div>
+        <div id="markdownBlank" innerhtml.bind='"" | md'></div>
+        <div id="markdown" innerhtml.bind='"# Hello" | md'></div>
+        <div id="phoneBlank">\${"" | phone}</div>
+        <div id="phone">\${phone | phone}</div>
+        <div id="phoneCountry">\${phoneLocal | phone:phoneCountry}</div>
+        <div id="phoneLocalBlank">\${"" | phoneLocal}</div>
+        <div id="phoneLocal">\${phone | phoneLocal}</div>
+        <div id="phoneLocalCountry">\${phoneLocal | phoneLocal:phoneCountry}</div>
+        <div id="phoneHtmlBlank" innerhtml.bind='"" | phoneHtml'></div>
+        <div id="phoneHtml" innerhtml.bind='phone | phoneHtml'></div>
+        <div id="phoneHtmlCountry" innerhtml.bind='phoneLocal | phoneHtml:phoneCountry'></div>
+        <div id="phoneHtmlLocalBlank" innerhtml.bind='"" | phoneLocalHtml'></div>
+        <div id="phoneHtmlLocal" innerhtml.bind='phone | phoneLocalHtml'></div>
+        <div id="phoneHtmlLocalCountry" innerhtml.bind='phoneLocal | phoneLocalHtml:phoneCountry'></div>
+        <div id="date">\${"2018-01-01T00:00:00.000" | date}</div>
+        <div id="time">\${"2018-01-01T00:00:00.000" | time}</div>
+        <div id="datetime">\${"2018-01-01T00:00:00.000" | datetime}</div>
+        <div id="utc">\${"2018-01-01T00:00:00.000" | utc}</div>
+        <div id="age">\${date | age}</div>
+        <div id="fromNow">\${date | fromnow}</div>
+        <div id="number">\${91824 | number}</div>
+        <div id="currency">\${91824 | currency}</div>
+        <div id="percent">\${92.4 | percent}</div>
         </ui-viewport>`
       )
       .boundTo({
@@ -47,74 +70,74 @@ describe("valueConverter/ui-text", () => {
     });
   });
 
-  // it("should format markdown", done => {
-  //   component.waitForElements("#markdownBlank, #markdown h1").then(el => {
-  //     expect(el[0].innerHTML).toBe("");
-  //     expect(el[1].innerHTML).toBe("Hello");
-  //     done();
-  //   });
-  // });
+  it("should format markdown", done => {
+    component.waitForElements("#markdownBlank, #markdown h1").then(el => {
+      expect(el[0].innerHTML).toBe("");
+      expect(el[1].innerHTML).toBe("Hello");
+      done();
+    });
+  });
 
-  // it("should format phone", done => {
-  //   component.waitForElements("#phoneBlank, #phone, #phoneCountry").then(el => {
-  //     expect(el[0].innerHTML).toBe("");
-  //     expect(el[2].innerHTML).toBe("+971 50 318 2424");
-  //     expect(el[1].innerHTML).toBe("+971 50 318 2424");
-  //     done();
-  //   });
-  // });
+  it("should format phone", done => {
+    component.waitForElements("#phoneBlank, #phone, #phoneCountry").then(el => {
+      expect(el[0].innerHTML).toBe("");
+      expect(el[2].innerHTML).toBe("+971 50 318 2424");
+      expect(el[1].innerHTML).toBe("+971 50 318 2424");
+      done();
+    });
+  });
 
-  // it("should format phone local", done => {
-  //   component.waitForElements("#phoneLocalBlank, #phoneLocal, #phoneLocalCountry").then(el => {
-  //     expect(el[0].innerHTML).toBe("");
-  //     expect(el[2].innerHTML).toBe("050 318 2424");
-  //     expect(el[1].innerHTML).toBe("050 318 2424");
-  //     done();
-  //   });
-  // });
+  it("should format phone local", done => {
+    component.waitForElements("#phoneLocalBlank, #phoneLocal, #phoneLocalCountry").then(el => {
+      expect(el[0].innerHTML).toBe("");
+      expect(el[2].innerHTML).toBe("050 318 2424");
+      expect(el[1].innerHTML).toBe("050 318 2424");
+      done();
+    });
+  });
 
-  // it("should format phone HTML", done => {
-  //   component
-  //     .waitForElements("#phoneHtmlBlank span, #phoneHtml span, #phoneHtmlCountry span")
-  //     .then(el => {
-  //       expect(el[0].className).toBe("ui-flag ");
-  //       expect(el[1].className).toBe("ui-flag AE");
-  //       expect(el[2].className).toBe("ui-flag AE");
-  //       done();
-  //     });
-  // });
+  it("should format phone HTML", done => {
+    component
+      .waitForElements("#phoneHtmlBlank span, #phoneHtml span, #phoneHtmlCountry span")
+      .then(el => {
+        expect(el[0].className).toBe("ui-flag ");
+        expect(el[1].className).toBe("ui-flag AE");
+        expect(el[2].className).toBe("ui-flag AE");
+        done();
+      });
+  });
 
-  // it("should format phone local HTML", done => {
-  //   component
-  //     .waitForElements(
-  //       "#phoneHtmlLocalBlank span, #phoneHtmlLocal span, #phoneHtmlLocalCountry span"
-  //     )
-  //     .then(el => {
-  //       expect(el[0].className).toBe("ui-flag ");
-  //       expect(el[1].className).toBe("ui-flag AE");
-  //       expect(el[2].className).toBe("ui-flag AE");
-  //       done();
-  //     });
-  // });
+  it("should format phone local HTML", done => {
+    component
+      .waitForElements(
+        "#phoneHtmlLocalBlank span, #phoneHtmlLocal span, #phoneHtmlLocalCountry span"
+      )
+      .then(el => {
+        expect(el[0].className).toBe("ui-flag ");
+        expect(el[1].className).toBe("ui-flag AE");
+        expect(el[2].className).toBe("ui-flag AE");
+        done();
+      });
+  });
 
-  // it("should format dates", done => {
-  //   component.waitForElements("#date, #time, #datetime, #utc, #age, #fromNow").then(el => {
-  //     expect(el[0].innerHTML).toBe("01 Jan 2018");
-  //     expect(el[1].innerHTML).toBe("12:00 AM");
-  //     expect(el[2].innerHTML).toBe("01 Jan 2018 12:00 AM");
-  //     expect(el[3].innerHTML).not.toBeNull();
-  //     expect(el[4].innerHTML).toBe("2 hours");
-  //     expect(el[5].innerHTML).toBe("about 2 hours ago");
-  //     done();
-  //   });
-  // });
+  it("should format dates", done => {
+    component.waitForElements("#date, #time, #datetime, #utc, #age, #fromNow").then(el => {
+      expect(el[0].innerHTML).toBe("01 Jan 2018");
+      expect(el[1].innerHTML).toBe("12:00 AM");
+      expect(el[2].innerHTML).toBe("01 Jan 2018 12:00 AM");
+      expect(el[3].innerHTML).not.toBeNull();
+      expect(el[4].innerHTML).toBe("2 hours");
+      expect(el[5].innerHTML).toBe("about 2 hours ago");
+      done();
+    });
+  });
 
-  // it("should format numbers", done => {
-  //   component.waitForElements("#number, #currency, #percent").then(el => {
-  //     expect(el[0].innerHTML).toBe("91,824");
-  //     expect(el[1].innerHTML).toBe("$ 91,824.00");
-  //     expect(el[2].innerHTML).toBe("92.40%");
-  //     done();
-  //   });
-  // });
+  it("should format numbers", done => {
+    component.waitForElements("#number, #currency, #percent").then(el => {
+      expect(el[0].innerHTML).toBe("91,824");
+      expect(el[1].innerHTML).toBe("$ 91,824.00");
+      expect(el[2].innerHTML).toBe("92.40%");
+      done();
+    });
+  });
 });
