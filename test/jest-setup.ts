1d18c97abf9d913caad3af17c746b1cf42929855
Adarsh Pastakia <adarsh.pastakia@pegasus.ae>
2019-05-14 11:21:22 +0400

    Fixed tests

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,10 +1,8 @@
 import { Options } from "aurelia-loader-nodejs";
 import { globalize } from "aurelia-pal-nodejs";
 import "aurelia-polyfills";
-import jsglobal from "jsdom-global";
 import * as path from "path";
 
 Options.relativeToDir = path.join(__dirname, "unit");
-jsglobal();
 globalize();
 
