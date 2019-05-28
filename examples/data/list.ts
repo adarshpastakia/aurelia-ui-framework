ae9777ebf996e143eb5f8016ecdf619f845bb7d5
Adarsh Pastakia <adarshpastakia@outlook.com>
2019-05-28 18:55:31 +0400

    Updated examples

--- examples/data/list.ts
+++ examples/data/list.ts
@@ -4,7 +4,10 @@
  * @copyright : 2019
  * @license   : MIT
  */
+import { autoinject } from "aurelia-framework";
+import { UINotificationService } from "aurelia-ui-framework";
 
+@autoinject()
 export class DatalistPage {
   protected actions = [
     { icon: "mdi mdi-pencil", label: "Edit" },
@@ -12,6 +15,80 @@ export class DatalistPage {
     "-",
     { icon: "mdi mdi-settings", iconColor: "gray", label: "Settings" }
   ];
+
+  protected ds = [{
+    title: "Speak to Me/Breathe",
+    album: "Dark Side of the Moon",
+    artist: "Pink Floyd",
+    duration: 3.15,
+    genre: "Psychedelic Rock",
+    year: 1972,
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Dark_Side_of_the_Moon.png/220px-Dark_Side_of_the_Moon.png",
+    members: "David Gilmour, Nick Mason, Roger Waters, Richard Wright"
+  }, {
+    title: "On the Run",
+    album: "Dark Side of the Moon",
+    artist: "Pink Floyd",
+    duration: 3.15,
+    genre: "Psychedelic Rock",
+    year: 1972,
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Dark_Side_of_the_Moon.png/220px-Dark_Side_of_the_Moon.png",
+    members: "David Gilmour, Nick Mason, Roger Waters, Richard Wright"
+  }, {
+    title: "Time/Breathe Reprise",
+    album: "Dark Side of the Moon",
+    artist: "Pink Floyd",
+    duration: 3.15,
+    genre: "Psychedelic Rock",
+    year: 1972,
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Dark_Side_of_the_Moon.png/220px-Dark_Side_of_the_Moon.png",
+    members: "David Gilmour, Nick Mason, Roger Waters, Richard Wright"
+  }, {
+    title: "Great Gig in the Sky",
+    album: "Dark Side of the Moon",
+    artist: "Pink Floyd",
+    duration: 3.15,
+    genre: "Psychedelic Rock",
+    year: 1972,
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Dark_Side_of_the_Moon.png/220px-Dark_Side_of_the_Moon.png",
+    members: "David Gilmour, Nick Mason, Roger Waters, Richard Wright"
+  }, {
+    title: "Money",
+    album: "Dark Side of the Moon",
+    artist: "Pink Floyd",
+    duration: 3.15,
+    genre: "Psychedelic Rock",
+    year: 1972,
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Dark_Side_of_the_Moon.png/220px-Dark_Side_of_the_Moon.png",
+    members: "David Gilmour, Nick Mason, Roger Waters, Richard Wright"
+  }, {
+    title: "Any Colour You Like",
+    album: "Dark Side of the Moon",
+    artist: "Pink Floyd",
+    duration: 3.15,
+    genre: "Psychedelic Rock",
+    year: 1972,
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Dark_Side_of_the_Moon.png/220px-Dark_Side_of_the_Moon.png",
+    members: "David Gilmour, Nick Mason, Roger Waters, Richard Wright"
+  }, {
+    title: "Brain Damage",
+    album: "Dark Side of the Moon",
+    artist: "Pink Floyd",
+    duration: 3.15,
+    genre: "Psychedelic Rock",
+    year: 1972,
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Dark_Side_of_the_Moon.png/220px-Dark_Side_of_the_Moon.png",
+    members: "David Gilmour, Nick Mason, Roger Waters, Richard Wright"
+  }, {
+    title: "Eclipse",
+    album: "Dark Side of the Moon",
+    artist: "Pink Floyd",
+    duration: 3.15,
+    genre: "Psychedelic Rock",
+    year: 1972,
+    image: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Dark_Side_of_the_Moon.png/220px-Dark_Side_of_the_Moon.png",
+    members: "David Gilmour, Nick Mason, Roger Waters, Richard Wright"
+  }];
 
   protected datalistHtml = `<template>
 
@@ -24,4 +101,21 @@ export class DatalistPage {
     attrs: [],
     events: []
   };
+
+
+  constructor(private notificationService: UINotificationService) {
+  }
+
+  protected notif(record: KeyValue) {
+    this.notificationService.toast({
+      message: `<ui-row>
+        <ui-icon round size="xl" ui-margin="sm"><img src="${record.image}"/></ui-icon>
+        <ui-col size="fill">
+        <p ui-font="md" ui-weight="medium">${record.title}</p>
+        <p ui-color="muted">${record.album}</p>
+        <p ui-font="sm" ui-weight="medium">${record.artist}</p>
+        </ui-col>`,
+      theme: "info"
+    });
+  }
 }
