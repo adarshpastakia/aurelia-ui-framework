9c47397e364f03bfe369f8e1f203255ea388673f
Adarsh Pastakia <adarshpastakia@outlook.com>
2019-05-29 06:32:30 +0400

    Minor fixes

--- src/buttons/ui-button-group.ts
+++ src/buttons/ui-button-group.ts
@@ -10,7 +10,7 @@ import {
   bindable,
   bindingMode,
   child,
-  children,
+  children, computedFrom,
   customElement,
   inlineView
 } from "aurelia-framework";
@@ -20,7 +20,7 @@ import { UIButton } from "./ui-button";
 @autoinject()
 @customElement("ui-button-group")
 @inlineView(
-  `<template class="ui-btn__group" click.delegate="buttonClicked($event)" data-disabled.bind="isDisabled" data-size.bind="size"><slot></slot></template>`
+  `<template class="ui-btn__group" click.delegate="buttonClicked($event)" data-disabled.bind="isDisabled" data-size.bind="size" data-type.bind="type"><slot></slot></template>`
 )
 export class UIButtonGroup {
   @bindable({ bindingMode: bindingMode.twoWay })
@@ -31,7 +31,7 @@ export class UIButtonGroup {
   @bindable()
   public size: "nm" | "sm" | "md" | "lg" = "nm";
   @bindable()
-  public type: "default" | "solid" | "link" = "default";
+  public type: "default" | "outline" | "solid" | "tool" = "default";
   @bindable()
   public disabled: boolean = false;
 
@@ -42,6 +42,8 @@ export class UIButtonGroup {
 
   private readonly toggle: boolean = false;
 
+  private elDisabled: boolean = false;
+
   constructor(protected element: Element) {
     if (element.hasAttribute("equal")) {
       element.classList.add("ui-btn__group--equal");
@@ -50,6 +52,15 @@ export class UIButtonGroup {
       element.classList.add("ui-btn__group--vertical");
     }
     this.toggle = element.hasAttribute("toggle");
+  }
+
+  @computedFrom("disabled", "elDisabled")
+  get isDisabled(): boolean {
+    return this.disabled || this.elDisabled;
+  }
+
+  public disable(disabled: boolean): void {
+    this.elDisabled = disabled;
   }
 
   protected attached(): void {
@@ -64,7 +75,6 @@ export class UIButtonGroup {
   protected buttonsChanged(): void {
     this.buttons.forEach(b => {
       (b.element as HTMLElement).dataset.separator = this.separator;
-      (b.element as HTMLElement).au.controller.viewModel.type = this.type;
     });
   }
 
