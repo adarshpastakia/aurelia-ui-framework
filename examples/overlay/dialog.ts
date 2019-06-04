08a89720f043b2829d57be110b15f3b8d14d6d96
Adarsh Pastakia <adarshpastakia@outlook.com>
2019-06-04 17:35:57 +0400

    Updated dialog example

--- examples/overlay/dialog.ts
+++ examples/overlay/dialog.ts
@@ -10,14 +10,28 @@ import { DlgTester } from "./dlgTester";
 
 @autoinject()
 export class DialogPage {
+  protected dialogHtml = `<template>
+  <ui-dialog label="My Dialog" help="true">
+    <ui-content ui-scroll ui-padding>
+      <lipsum-para></lipsum-para>
+    </ui-content>
+  </ui-dialog>
+</template>`;
   protected dialogJs = `import { UIDialogService } from "aurelia-ui-framework";
+
+@autoinject()
+export class MyDialog {
+  
+}`;
+  protected pageJs = `import { UIDialogService } from "aurelia-ui-framework";
+import { MyDialog } from "./dialog";
 
 @autoinject()
 export class PageVm {
   constructor(private dialogService: UIDialogService) {}
 
   fn() {
-    this.dialogService.open();
+    this.dialogService.open(MyDialog);
   }
 }`;
 
