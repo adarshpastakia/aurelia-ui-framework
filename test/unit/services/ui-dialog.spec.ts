c85e6c28bb0fd186e4870a7549a53060be521c4b
Adarsh Pastakia <adarshpastakia@outlook.com>
2019-06-04 17:29:59 +0400

    Updated tests

--- test/unit/services/ui-dialog.spec.ts
+++ test/unit/services/ui-dialog.spec.ts
@@ -6,33 +6,39 @@
  */
 
 import { bootstrap } from "aurelia-bootstrapper";
-import { Container, inlineView } from "aurelia-framework";
+import { Container } from "aurelia-framework";
 import { StageComponent } from "aurelia-testing";
 import { UIDialogService } from "aurelia-ui-framework";
+import { UIInternal } from "../../../src/utils/ui-internal";
 import { auconfig } from "../../jest-pretest";
-
-@inlineView(`<template>
-<ui-dialog icon="head-icon" label="Header Title">
-<ui-content>Tests</ui-content>
-</ui-dialog>
-</template>`)
-class MyDialog {}
+import {
+  mockActivate,
+  mockAttached,
+  mockBind,
+  mockCanActivate,
+  mockCanDeactivate,
+  mockDeactivate, mockDetached, mockUnbind,
+  MyDialog
+} from "./dialog";
 
 describe("ui-dialog", () => {
   let component;
 
-  const vm = {};
+  const vm = { title: "My Dialog", icon: "my-icon" };
 
   beforeAll(async () => {
     component = StageComponent.withResources()
       .inView(`<div><ui-viewport></ui-viewport></div>`)
-      .boundTo(vm);
+      .boundTo({});
 
     component.bootstrap(aurelia => {
       auconfig(aurelia);
     });
 
     await component.create(bootstrap);
+
+    const dialogService: UIDialogService = Container.instance.get(UIDialogService);
+    dialogService.open(MyDialog, vm);
   });
 
   afterAll(() => {
@@ -40,14 +46,37 @@ describe("ui-dialog", () => {
   });
 
   it("should have open dialog", done => {
-    const dialogService: UIDialogService = Container.instance.get(UIDialogService);
-    dialogService.open(MyDialog);
-
-    component.waitForElement(".ui-dialog").then(el => {
+    component.waitForElement("#dialog").then(el => {
       expect(el).not.toBeNull();
-      expect(el.querySelector(".ui-header__icon .ui-icon").className).toContain("head-icon");
-      expect(el.querySelector(".ui-header__title").textContent).toBe("Header Title");
+      expect(el.querySelector(".ui-dialog")).not.toBeNull();
+      expect(el.querySelector(".ui-header__title").textContent).toBe(vm.title);
+      expect(el.querySelector(".ui-header__icon .ui-icon").className).toContain(vm.icon);
       done();
+    });
+  });
+
+  it("should call open lifecycle", done => {
+    component.waitForElement("#dialog").then(el => {
+      expect(mockCanActivate).toBeCalled();
+      expect(mockActivate).toBeCalled();
+      expect(mockActivate).toBeCalledWith(vm);
+      expect(mockBind).toBeCalled();
+      expect(mockAttached).toBeCalled();
+      done();
+    });
+  });
+
+  it("should call close lifecycle", done => {
+    component.waitForElement("#dialog").then(el => {
+      getViewModel(el).close("YES");
+      UIInternal.queueMicroTask(() => {
+        expect(mockCanDeactivate).toBeCalled();
+        expect(mockCanDeactivate).toBeCalledWith("YES");
+        expect(mockDeactivate).toBeCalled();
+        expect(mockDetached).toBeCalled();
+        expect(mockUnbind).toBeCalled();
+        done();
+      });
     });
   });
 });
